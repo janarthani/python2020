fa86081cba8d998b8972982abb12e8107a72fcc4
Janu Dhayanathan <janudhayanathan@Janus-MacBook-Pro.local>
2020-06-22 16:51:54 +1000

    recommmit after removing folders

--- while.py
+++
@@ -1,5 +0,0 @@
-nunu = 0
-while nunu <= 100:
-    print(nunu)
-    nunu = nunu + 5
-print ("done")
--- while_3.py
+++
@@ -1,7 +0,0 @@
-x = 0
-while x <= 100:
-    print (x)
-    x = x + 1
-    if x ==95:
-        print ("almost done")
-print ("I am done")
